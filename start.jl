6dacdd141625067f2d8dcc0ac09f93e8f0b64193
Lion-Mod <70057706+Lion-Mod@users.noreply.github.com>
2021-06-18 08:09:32 +0100

    Last nights work, got data + tidied to US data only.

---
+++ start.jl
@@ -0,0 +1,39 @@
+using DataFrames:columns
+using DataFrames
+using CSV
+using Missings
+using Chain
+using Plots
+using Dates
+
+
+function getAndReadCovidData(url::String, filename::String)
+    download(url, filename)
+    return DataFrame(CSV.File(filename))
+end
+
+function renameColumns(df::DataFrame)
+    return @chain df begin
+        rename!(1 => "province",
+                2 => "country",
+                3 => "latitude",
+                4 => "longitude")
+    end
+end
+
+function getUSData(df::DataFrame)
+    return @chain df begin
+        filter(col -> col.country == "US", _)[:, 5:end]
+        stack(_, names(_))
+    end
+end
+
+url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+
+data = getAndReadCovidData(url, "covid_data.csv");
+tidy_data = renameColumns(data)
+US_data = getUSData(tidy_data)
+
+
+# Plot on log scale
+plot(log.(US_data[:, 2]))
